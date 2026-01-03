55f27d8867f7324e13b362e1b1bafa2ee5d74731
ZiloTech <uwamimcdonald@gmail.com>
2026-01-03 18:27:29 +0200

    electron

---
+++ webtoapk-electron/src/preload.js
@@ -0,0 +1,9 @@
+const { contextBridge, ipcRenderer } = require('electron');
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  selectFolder: () => ipcRenderer.invoke('select-folder'),
+  selectFile: (filters) => ipcRenderer.invoke('select-file', filters),
+  saveFile: (defaultPath) => ipcRenderer.invoke('save-file', defaultPath),
+  readFile: (filePath) => ipcRenderer.invoke('read-file', filePath),
+  writeFile: (filePath, content) => ipcRenderer.invoke('write-file', filePath, content)
+});
